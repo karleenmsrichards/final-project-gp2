e2ad3aba8dfec8df6af5c42f3c77b95b2d4f8d2a
tony-devops <ta.devopsuk@gmail.com>
2023-07-30 03:03:49 +0100

    Updated Home and Header Component

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,14 +1,15 @@
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Box, Button } from "@mui/material";
 import React from "react";
 
-export default function Header() {
-  const testHeader = () => {
-    return <Toolbar>This will be our Header</Toolbar>;
-  };
-  
+const Header = () => {
   return (
-    <header>
-      <AppBar>{testHeader()}</AppBar>
-    </header>
-  );
-}
+		<AppBar sx={{ background: "white" }}>
+			<Box sx={{ display: "flex", justifyContent: "flex-end", py: 2, px: 5, gap: 5 }}>
+				<Button variant="outlined">Sign Up</Button>
+				<Button variant="contained" >Log in</Button>
+			</Box>
+		</AppBar>
+	);
+};
+
+export default Header;

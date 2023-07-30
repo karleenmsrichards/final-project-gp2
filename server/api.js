425f089bf26476923e69b543b463befea0307e22
Karleen Richards <karleenmsrichards@gmail.com>
2023-07-30 18:28:31 +0100

    add server code to fetch clientId

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-
 import logger from "./utils/logger";
+const dotenv = require("dotenv");
+dotenv.config();
 
 const router = Router();
 
@@ -8,5 +9,18 @@ router.get("/", (_, res) => {
 	logger.debug("Welcoming everyone...");
 	res.json({ message: "Hello, world!" });
 });
+
+router.get("/clientId", (req, res) => {
+	try {
+	const clientId = process.env.REACT_APP_CLIENT_ID;
+	if (!clientId) {
+		throw new Error("Client ID not found.");
+	}
+res.json({ clientId }).status(200);
+	} catch (error) {
+	logger.error("Error fetching clientId:", error.message);
+	res.status(500).json({ error: "Internal server error" });
+	}
+  });
 
 export default router;

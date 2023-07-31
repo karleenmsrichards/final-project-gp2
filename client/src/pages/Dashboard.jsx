21d41d4a881233f0c740bc04a25f471e3ded6bf0
Hengameh M.Khani <afsooneday@gmail.com>
2023-08-01 00:29:25 +0100

    jwtToken

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,9 +2,28 @@ import { Container, Typography, Button } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+// KHOoDam
+
 const Dashboard = () => {
     const navigate = useNavigate();
     const [isLoggedIn,setIsLoggedIn]= useState(false);
+
+  // Retrieve the token from local storage
+  const token = localStorage.getItem("jwtToken");
+  // Check if the token exists in local storage
+  if (token) {
+      // The token exists, you can use it as needed
+      setIsLoggedIn(true);
+  } else {
+    // The token doesn't exist in local storage
+    console.log("Token not found");
+
+  }
+
+
+
+
+// KHOdaM
 
     function userIsLoggedIn(){
       // const value = get from localstorage or cookies

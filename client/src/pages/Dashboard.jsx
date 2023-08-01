2b22edf14e6737b8f4ed66ff7b58f06c28834b46
Hengameh M.Khani <afsooneday@gmail.com>
2023-08-01 13:06:38 +0100

    env.update

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { Container, Typography, Button } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 // KHOoDam
@@ -8,28 +8,22 @@ const Dashboard = () => {
     const navigate = useNavigate();
     const [isLoggedIn,setIsLoggedIn]= useState(false);
 
-  // Retrieve the token from local storage
-  const token = localStorage.getItem("jwtToken");
-  // Check if the token exists in local storage
-  if (token) {
-      // The token exists, you can use it as needed
-      setIsLoggedIn(true);
-  } else {
-    // The token doesn't exist in local storage
-    console.log("Token not found");
-
-  }
-
-
+    useEffect(() => {
+      // Retrieve the token from local storage
+      const token = localStorage.getItem("jwtToken");
+      // Check if the token exists in local storage
+      if (token) {
+        // The token exists, set the isLoggedIn state to true
+        setIsLoggedIn(true);
+      } else {
+        // The token doesn't exist in local storage
+        console.log("Token not found");
+      }
+    }, []);
 
 
 // KHOdaM
 
-    function userIsLoggedIn(){
-      // const value = get from localstorage or cookies
-     setIsLoggedIn(true);
-    }
-    userIsLoggedIn();
 
   return (
     <Container sx={{ width: "400px", height: "200px", marginTop: "200px" }}>

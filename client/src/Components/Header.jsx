a0af2e3cc1af52d0cb366e374fa263a55c2775a9
tony-devops <ta.devopsuk@gmail.com>
2023-08-02 17:39:32 +0100

    Updating Button for login out

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,33 +1,60 @@
 import { AppBar, Box, Button } from "@mui/material";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import useAuth from "../customHooks/useAuth";
 
 const Header = () => {
 	const navigate = useNavigate();
-	const { user, handleSignUp } = useAuth();
-  return (
-    <AppBar sx={{ background: "white" }}>
-      <Box sx={{ display: "flex", justifyContent: "flex-end", py: 2, px: 5, gap: 5 }}>
-        { !user && (
-          <Box>
-            <Button variant="outlined" onClick={handleSignUp}>
-              Sign Up
-            </Button>
-            <Button variant="outlined" onClick={handleSignUp}>
-              Sign In
-            </Button>
-          </Box>
-        )}
-        <Box id="signInDiv"></Box>
-        {user && (
-          <Button variant="outlined" onClick={() => navigate("/dashboard")}>
-            Dashboard
-          </Button>
-        )}
-      </Box>
-    </AppBar>
-  );
+	const location = useLocation();
+	const { handleSignUp, handleSignOut, isLoggedIn } = useAuth();
+
+	return (
+		<AppBar sx={{ background: "white" }}>
+			<Box
+				sx={{
+					display: "flex",
+					justifyContent: "flex-end",
+					py: 2,
+					px: 5,
+					gap: 5,
+				}}
+			>
+				{location.pathname === "/" && (
+					<>
+						{!isLoggedIn ? (
+							<Box id="signInDiv">
+								<Button variant="outlined" onClick={handleSignUp}>
+									Sign In
+								</Button>
+							</Box>
+						) : (
+							<>
+								<Button
+									variant="outlined"
+									onClick={() => navigate("/dashboard")}
+								>
+									Dashboard
+								</Button>
+								<Button variant="outlined" onClick={handleSignOut}>
+									Sign Out
+								</Button>
+							</>
+						)}
+					</>
+				)}
+				{location.pathname === "/dashboard" && (
+					<>
+						<Button variant="outlined" onClick={() => navigate("/")}>
+							Home
+						</Button>
+						<Button variant="outlined" onClick={handleSignOut}>
+							Sign Out
+						</Button>
+					</>
+				)}
+			</Box>
+		</AppBar>
+	);
 };
 
 export default Header;
--- client/src/customHooks/useAuth.js
+++ client/src/customHooks/useAuth.js
@@ -28,16 +28,19 @@ const useAuth = () => {
 		navigate("/");
 	};
 
-  function handleCallbackResponse(response) {
-    if (response && response.credential) {
-      let userObject = jwtDecode(response.credential);
-      console.log(userObject);
-      localStorage.setItem("jwtToken", response.credential);
-      navigate("/dashboard");
-    } else {
-      console.error("Error handling callback response:", response);
-    }
-  }
+	function handleCallbackResponse(response) {
+		if (response && response.credential) {
+			// console.log("Encoded JWT ID token: " + response.credential);
+			let userObject = jwtDecode(response.credential);
+			// console.log(userObject);
+			localStorage.setItem("jwtToken", response.credential);
+			setIsLoggedIn(true); // Update isLoggedIn to true after successful login
+			navigate("/dashboard");
+		} else {
+			console.error("Error handling callback response:", response);
+		}
+	}
+
 	useEffect(() => {
 		async function fetchClientId() {
 			try {
@@ -51,20 +54,24 @@ const useAuth = () => {
 		fetchClientId();
 	}, []);
 
-  useEffect(() => {
-    const token = localStorage.getItem("jwtToken");
+	useEffect(() => {
+		const token = localStorage.getItem("jwtToken");
 		if (token !== null && token !== "") {
 			const userObject = jwtDecode(token);
 			const currentTime = Date.now() / 1000;
 
 			if (userObject.exp > currentTime) {
 				setUser(userObject);
+				setIsLoggedIn(true); // Update isLoggedIn to true if valid token is present
 			} else {
 				setUser(null);
 				localStorage.removeItem("jwtToken");
+				setIsLoggedIn(false); // Update isLoggedIn to false if token is expired
 			}
+		} else {
+			setIsLoggedIn(false); // Update isLoggedIn to false if no token is found
 		}
-  }, [navigate]);
+	}, [navigate]);
 
 	return { user, handleSignUp, handleSignOut, isLoggedIn, setIsLoggedIn };
 };

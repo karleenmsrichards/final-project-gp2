7b4f984fbf4bc348ff39ed263836434aff6dd63c
farzaneh-haghani <farzaneh.haghani@gmail.com>
2023-08-04 06:36:44 +0100

    sequelize-migration installed

---
+++ server/sequelize/migrations/20230804001548-create-tokens-table.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Tokens", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      token: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Tokens");
+  },
+};
---
+++ server/sequelize/models/tokens.js
@@ -0,0 +1,23 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Tokens extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Tokens.init({
+    token: DataTypes.TEXT,
+    user_id: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: "Tokens",
+  });
+  return Tokens;
+};

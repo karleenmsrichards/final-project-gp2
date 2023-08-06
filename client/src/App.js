5fe4260373e75da3522cffcc9f9c4094f44711a0
Karleen Richards <karleenmsrichards@gmail.com>
2023-08-06 20:17:35 +0100

    add path to sign-up form page

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,19 @@ import ContactUs from "./pages/ContactUs";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Dashboard from "./pages/Dashboard";
+import SignUpForm from "./pages/SignUpForm";
 
 const App = () => (
-	<div>
-		<Header />
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/dashboard" element={<Dashboard />} />
-			<Route path="/contact-us" element={<ContactUs />} />
-		</Routes>
-		<Footer />
-	</div>
+  <div>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-up" element={<SignUpForm />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/contact-us" element={<ContactUs />} />
+    </Routes>
+    <Footer />
+  </div>
 );
 
 export default App;

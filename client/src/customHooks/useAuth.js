50ccbfb25caa3bcd4370961479a4e8c602a7d912
Karleen Richards <karleenmsrichards@gmail.com>
2023-08-10 09:24:01 +0100

    ran lint:fix

--- client/src/customHooks/useAuth.js
+++ client/src/customHooks/useAuth.js
@@ -4,74 +4,82 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const useAuth = () => {
-  const navigate = useNavigate();
-  const [user, setUser] = useState(null);
-  const [clientId, setClientId] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [ isProvider, setIsProvider ] = useState(false);
+	const navigate = useNavigate();
+	const [user, setUser] = useState(null);
+	const [clientId, setClientId] = useState("");
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isProvider, setIsProvider] = useState(false);
 
-  function handleSignUp() {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: clientId,
-      callback: handleCallbackResponse,
-    });
-    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
-      theme: "outline",
-      size: "large",
-    });
-    google.accounts.id.prompt();
-  }
+	function handleSignUp() {
+		/* global google */
+		google.accounts.id.initialize({
+			client_id: clientId,
+			callback: handleCallbackResponse,
+		});
+		google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+			theme: "outline",
+			size: "large",
+		});
+		google.accounts.id.prompt();
+	}
 
-  const handleSignOut = () => {
-    localStorage.removeItem("jwtToken");
-    setIsLoggedIn(false);
-    navigate("/");
-  };
+	const handleSignOut = () => {
+		localStorage.removeItem("jwtToken");
+		setIsLoggedIn(false);
+		navigate("/");
+	};
 
-  function handleCallbackResponse(response) {
-    if (response && response.credential) {
-      localStorage.setItem("jwtToken", response.credential);
-      setIsLoggedIn(true);
-      navigate("/dashboard");
-    } else {
-      console.error("Error handling callback response:", response);
-    }
-  }
+	function handleCallbackResponse(response) {
+		if (response && response.credential) {
+			localStorage.setItem("jwtToken", response.credential);
+			setIsLoggedIn(true);
+			navigate("/dashboard");
+		} else {
+			console.error("Error handling callback response:", response);
+		}
+	}
 
-  useEffect(() => {
-    async function fetchClientId() {
-      try {
-        const response = await axios.get("/api/clientId");
-        const { clientId } = response.data;
-        setClientId(clientId);
-      } catch (error) {
-        console.error("Error fetching client ID:", error);
-      }
-    }
-    fetchClientId();
-  }, []);
+	useEffect(() => {
+		async function fetchClientId() {
+			try {
+				const response = await axios.get("/api/clientId");
+				const { clientId } = response.data;
+				setClientId(clientId);
+			} catch (error) {
+				console.error("Error fetching client ID:", error);
+			}
+		}
+		fetchClientId();
+	}, []);
 
-  useEffect(() => {
-    const token = localStorage.getItem("jwtToken");
-    if (token !== null && token !== "") {
-      const userObject = jwtDecode(token);
-      const currentTime = Date.now() / 1000;
+	useEffect(() => {
+		const token = localStorage.getItem("jwtToken");
+		if (token !== null && token !== "") {
+			const userObject = jwtDecode(token);
+			const currentTime = Date.now() / 1000;
 
-      if (userObject.exp > currentTime) {
-        setUser(userObject);
-        setIsLoggedIn(true);
-      } else {
-        setUser(null);
-        localStorage.removeItem("jwtToken");
-        setIsLoggedIn(false);
-      }
-    } else {
-      setIsLoggedIn(false);
-    }
-  }, [navigate]);
+			if (userObject.exp > currentTime) {
+				setUser(userObject);
+				setIsLoggedIn(true);
+			} else {
+				setUser(null);
+				localStorage.removeItem("jwtToken");
+				setIsLoggedIn(false);
+			}
+		} else {
+			setIsLoggedIn(false);
+		}
+	}, [navigate]);
 
-  return { user, handleSignUp, handleSignOut, isLoggedIn, setIsLoggedIn, isProvider, setIsProvider };
+	return {
+		user,
+		handleSignUp,
+		handleSignOut,
+		isLoggedIn,
+		setIsLoggedIn,
+		isProvider,
+		setIsProvider,
+	};
 };
 
 export default useAuth;
